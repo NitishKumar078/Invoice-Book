3735ee965de065699a10614a7bf88a5829fd244f
nitish kumar <nishkumar562@gmail.com>
2025-06-02 16:59:54 +0530

    Refactor App component: remove unused Clerk authentication code and clean up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SidebarLayout from './components/SidebarLayout';
 import DashBoard from './pages/DashBoard';
 import InvoiceGenerator from './pages/InvoiceGenerator';
@@ -11,28 +6,10 @@ import ViewInvoice from './pages/ViewInvoice';
 import ListInvoices from './pages/Listing/ListInvoices';
 import ListItems from './pages/Listing/ListItems';
 import Listcustomers from './pages/Listing/Listcustomers';
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-  useUser,
-} from '@clerk/clerk-react';
-import { useEffect } from 'react';
 import Payment from './pages/Payment';
 
 function App() {
-  // const { user } = useUser();
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (user?.publicMetadata?.trialExpired) {
-  //     navigate('/upgrade-page'); // Redirect users whose trial has expired
-  //   }
-  // }, [user, navigate]);
   return (
-    // <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-    //   <SignedIn>
     <Router>
       <SidebarLayout>
         <Routes>
@@ -46,11 +23,6 @@ function App() {
         </Routes>
       </SidebarLayout>
     </Router>
-    //   </SignedIn>
-    //   <SignedOut>
-    //     <RedirectToSignIn />
-    //   </SignedOut>
-    // </ClerkProvider>
   );
 }
 
